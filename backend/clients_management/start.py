7bd472332bd831d5736312244603c4759637efe2
alex6712 <alexeivanyukov@yandex.ru>
2025-04-14 19:29:54 +0300

    Фикс подключения к базе данных №5

--- backend/clients_management/start.py
+++ backend/clients_management/start.py
@@ -1,14 +1,28 @@
 import asyncio
+import asyncpg
 
 import uvicorn
 
 from core.config import Settings, get_settings
 from database import initialize
 
+
+async def wait_for_db():
+    for _ in range(10):
+        try:
+            conn = await asyncpg.connect(settings.DATABASE_URL)
+            await conn.close()
+            return True
+        except Exception as _:
+            await asyncio.sleep(2)
+    return False
+
+
 if __name__ == "__main__":
     settings: Settings = get_settings()
 
-    print(settings.DATABASE_URL)
+    if not asyncio.run(wait_for_db()):
+        raise RuntimeError(f"Не удалось подключиться к БД. Строка подключения: {settings.DATABASE_URL}")
 
     if settings.INITIALIZE_DB:
         asyncio.run(initialize())
