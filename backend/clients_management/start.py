280d6005f92f469f30c002f2602499a1ece847d8
alex6712 <alexeivanyukov@yandex.ru>
2025-04-14 19:35:40 +0300

    Фикс подключения к базе данных №7

--- backend/clients_management/start.py
+++ backend/clients_management/start.py
@@ -6,15 +6,19 @@ import uvicorn
 from core.config import Settings, get_settings
 from database import initialize
 
+STACK_TRACE = set
+
 
 async def wait_for_db():
+    global STACK_TRACE
+
     for _ in range(10):
         try:
             conn = await asyncpg.connect(settings.DATABASE_URL)
             await conn.close()
             return True
         except Exception as e:
-            print(e)
+            STACK_TRACE.add(str(e))
             await asyncio.sleep(2)
     return False
 
@@ -23,7 +27,7 @@ if __name__ == "__main__":
     settings: Settings = get_settings()
 
     if not asyncio.run(wait_for_db()):
-        raise RuntimeError(f"Не удалось подключиться к БД. Строка подключения: {settings.DATABASE_URL}")
+        raise RuntimeError(f"Не удалось подключиться к БД. Ошибки: {STACK_TRACE}")
 
     if settings.INITIALIZE_DB:
         asyncio.run(initialize())
